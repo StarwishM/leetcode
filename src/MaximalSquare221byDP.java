aab4aaf7a3b4aa5096444b370cedf5a0ab18c717
starwishm <852474811@qq.com>
2022-09-16 16:49:47 +0800

    q221 ans (by DP)try 2

---
+++ src/MaximalSquare221byDP.java
@@ -0,0 +1,12 @@
+public class MaximalSquare221byDP {
+    public static void main(String[] args) {
+        char[][] m = {{'1', '0', '1', '0', '0'}, {'1', '0', '1', '1', '1'}, {'1', '1', '1', '1', '1'}, {'1', '0', '0', '1', '0'}};
+        System.out.println(maximalSquare(m));
+    }
+
+    private static int maximalSquare(char[][] matrix) {
+        int maxSquareSide = 0;
+            System.out.println(maxSquareSide);
+        return maxSquareSide;
+    }
+}
